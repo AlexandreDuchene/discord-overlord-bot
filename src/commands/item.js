8fb97803066c784fb7932c8bf000304963571d01
Prof. Chêne <alexandre.duch3ne@gmail.com>
2021-05-10 01:06:37 +0200

    WIP item commande

--- src/commands/item.js
+++ src/commands/item.js
@@ -1,8 +1,27 @@
-const apiEndPoint = 'https://fr.classic.wowhead.com';
+const Discord = require('discord.js');
+const wowheadFetch = require('wowhead-fetch');
+const parser = wowheadFetch.parser;
+const iconEndPoint = 'https://wow.zamimg.com/images/wow/icons/large/';
 
 exports.executeCommand = function(message)
 {
+    const words = message.content.split(' ');
 
+    wowheadFetch.apis.classic.fetchItem(words[1], 'fr')
+        .then((item) => {
+            const name = item.name.toString();
+            const url = item.link.toString();
+            const image = iconEndPoint + item.icon[0]._ + '.jpg';
+            const json = parser.extractJsonData(item);
+
+            console.log(json);
+            embedMessage = new Discord.MessageEmbed()
+                .setTitle(name)
+                .setURL(url)
+                .setThumbnail(image)
+            ;
+            message.channel.send(embedMessage);
+        });
 }
 
 exports.help = "work in progress - be patient :)";
